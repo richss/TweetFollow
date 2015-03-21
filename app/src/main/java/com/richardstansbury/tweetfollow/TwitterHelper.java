19ef606beefe89abd53991189c5a1c72996b2159
Richard Stansbury <stansbur@erau.edu>
2015-03-20 22:17:45 -0400

    Refactoring and commenting.

--- app/src/main/java/com/richardstansbury/tweetfollow/TwitterHelper.java
+++ app/src/main/java/com/richardstansbury/tweetfollow/TwitterHelper.java
@@ -9,12 +9,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 import twitter4j.*;
+import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
  * @author Richard S. Stansbury
  *
- * This is a class to handle Twitter4J method calls to provide twitter functionality for app.
+ * This is a support class to handle interactions with Twitter4J API for Twiter.
+ *
+ * It creates a _twitter stream class that will can be started using an array of keywords.
+ *
+ * It also implements the listener, which will receive the _tweets and add them to an array
+ * list at location (0), i.e. front of the list.
+ *
+ * This assumes that it is being created by the main activity and receives the main
+ * activity as its _parent.  This is necessary to 1) access the assets folder for configuration
+ * and 2) allow the onStatus listener to trigger a UI update on the UI thread.
+ *
+ * Please see the readme.txt file in the root folder to learn how to configure your
+ * assets/twitter4j.txt file for your Twitter API keys.
  */
 
 /* The MIT License (MIT)
@@ -39,29 +52,52 @@ import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterHelper implements StatusListener {
 
-    TwitterStream twitter;
-    List<String> tweets;
-    Activity parent;
-    ArrayAdapter<String> adapter;
+    private TwitterStream _twitter;
+    private List<String> _tweets;
+    private Activity _parent;
+    private ArrayAdapter<String> _adapter;
 
-    public TwitterHelper(List<String> tweets, Activity parentActivity, ArrayAdapter<String> adapter)
+
+    /**
+     * Constructor
+     * @param tweets - an array list to store _tweets received by this class
+     * @param parentActivity - _parent activity that this helper is running on
+     * @param adapter - array _adapter that is being updated by this class.
+     */
+    public TwitterHelper(List<String> tweets,
+                         Activity parentActivity,
+                         ArrayAdapter<String> adapter)
     {
-        ConfigurationBuilder cb = readConfiguration(parentActivity);
-        this.twitter = new TwitterStreamFactory(cb.build()).getInstance();
-        this.tweets = tweets;
-        this.parent = parentActivity;
-        this.adapter = adapter;
-        twitter.addListener(this);
+        //Set up the Twitter Stream FActor and listener.
+        this._twitter = new TwitterStreamFactory(readConfiguration(parentActivity)).getInstance();
+        this._twitter.addListener(this);
+
+        //Copy parameters to class variables
+        this._tweets = tweets;
+        this._parent = parentActivity;
+        this._adapter = adapter;
     }
 
-    private ConfigurationBuilder readConfiguration(Context c)
+    /**
+     * Creates an authentication configuration for accessing the _twitter streaming
+     * API.
+     *
+     * See readme.txt in root folder for location and format of twitter4j.txt.
+     *
+     * @param c - context to access the asset's folder
+     * @return configuration object based upon keys read from asset's folder
+     */
+    private Configuration readConfiguration(Context c)
     {
 
+        //Create a Configuration Builder
         ConfigurationBuilder cb = new ConfigurationBuilder();
         try {
+
+            //Create an input stream for the properties file
             BufferedReader in = new BufferedReader(new InputStreamReader(c.getAssets().open("twitter4j.txt")));
 
-
+            //Set up the configuration
             cb.setDebugEnabled(true)
                     .setOAuthConsumerKey(in.readLine())
                     .setOAuthConsumerSecret(in.readLine())
@@ -73,28 +109,48 @@ public class TwitterHelper implements StatusListener {
             Log.e("TwitterHelper", "Error opening input file");
             System.exit(-1);
         }
-        return cb;
+
+        //Build the configuration and return the object
+        return cb.build();
     }
 
+    /**
+     * Starts a filter query of the Twitter Streaming API.
+     *
+     * @param keywords array of string keywords
+     */
     public void start(String [] keywords) {
-        twitter.filter((new FilterQuery()).track(keywords));
+        _twitter.filter((new FilterQuery()).track(keywords));
     }
 
+    /**
+     * Stops the filter query of the Twitter Streaming API
+     */
     public void stop() {
-        twitter.cleanUp();
+        _twitter.cleanUp();
     }
 
+    /**
+     * Process's received Twitter status updates based upon the filter query.
+     *
+     * Implemented as part of the Status Listener Class.
+     * @param status - a status update received by the listener.
+     */
     @Override
     public void onStatus(Status status) {
         String tweetStr = status.getUser().getScreenName() + ": " + status.getText() + "(" + status.getCreatedAt() + ")";
-        tweets.add(0, tweetStr);
-        parent.runOnUiThread(new Runnable() {
+        _tweets.add(0, tweetStr);
+        _parent.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                adapter.notifyDataSetChanged();
+                _adapter.notifyDataSetChanged();
             }
         });
     }
+
+
+    //////////////////////////
+    //  Unused methods from the StatusListener interface
 
     @Override
     public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
